8027625c733c724162d86980ec80e1b8cc5431fb
Akshat Shah <shahakshat1511@gmail.com>
2025-02-08 19:32:44 +0530

    Updated

---
+++ Aupare/View/AupareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AupareApp.swift
+//  Aupare
+//
+//  Created by Akshat on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AupareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
